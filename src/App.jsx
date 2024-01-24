ffcb06af1ffb7cb7969a47528438fbeb6e8e0f85
tjdwns335 <http://github.com/tjdwns335>
2024-01-24 12:40:38 +0900

    defaultStyleRemove

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,13 @@
-import './App.css';
-import styled from 'styled-components';
-import TestPage from './components/TestPage';
-import GlobalStyle from './GlobalStyle2';
-
-const StContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`
-
-const StBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 1px solid ${(props) => props.borderColor};
-  margin: 20px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
-// 박스의 색
-const boxList = ['red', 'blue', 'green', 'black'];
-
-// 색을 넣으면, 이름을 반환
-const getBoxName = (color) => {
-  switch (color) {
-    case 'red':
-      return "빨간 박스";
-    case 'green':
-      return '초록 박스';
-    case 'blue':
-      return '파란박스';
-    default:
-      return '검정 박스';
-  }
-}
+import React from 'react'
 
 function App() {
   return (
-    // <StContainer>
-    //   {/* <StBox borderColor="red">빨간박스</StBox> */}
-    //   {
-    //     boxList.map((box) => {
-    //       return <StBox borderColor={box}> {getBoxName(box)}</StBox>
-    //     })
-    //   }
-    // </StContainer >
-    <>
-      <GlobalStyle />
-      <TestPage
-        title="제목입니다"
-        contents="내용입니다"
-      ></TestPage>
-    </>
-  );
+    <div>
+      <span>Default Style 테스트</span>
+      <h1>이건 h1 태그입니다</h1>
+      <p>나는 p 태그입니다.</p>
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/GlobalStyle2.jsx
+++
@@ -1,9 +0,0 @@
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle2 = createGlobalStyle`
-  body{
-    font-family: "Helvetica", "Arial", sans-serif;
-    line-height: 1.5;
-  }
-`
-export default GlobalStyle2;
