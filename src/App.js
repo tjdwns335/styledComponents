9db07c343a3aa68fb1f177ad5e3de62e572cc31d
tjdwns335 <http://github.com/tjdwns335>
2024-01-24 11:33:16 +0900

    styled-components test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const StBox = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 1px solid red;
+  margin: 20px;
+`;
+const StP = styled.p`
+color:blue;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StBox>
+      <StP>나는 p Tag입니다.</StP>
+    </StBox>
   );
 }
 
