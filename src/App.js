47eec8ddb4ecc11723519ff8beb169c0e8692fcd
tjdwns335 <http://github.com/tjdwns335>
2024-01-24 11:38:56 +0900

    조건부 style

--- src/App.js
+++ src/App.js
@@ -4,18 +4,17 @@ import styled from 'styled-components';
 const StBox = styled.div`
   width: 100px;
   height: 100px;
-  border: 1px solid red;
+  border: 1px solid ${(props) => props.borderColor};
   margin: 20px;
 `;
-const StP = styled.p`
-color:blue;
-`
 
 function App() {
   return (
-    <StBox>
-      <StP>나는 p Tag입니다.</StP>
-    </StBox>
+    <>
+      <StBox borderColor="red">빨간박스</StBox>
+      <StBox borderColor="blue">파란박스</StBox>
+      <StBox borderColor="green">초록박스</StBox>
+    </>
   );
 }
 
