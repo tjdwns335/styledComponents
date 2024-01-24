affddc1d59f010409e732e2730ad3897a278e590
tjdwns335 <http://github.com/tjdwns335>
2024-01-24 11:41:41 +0900

    조건부 style

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ const StBox = styled.div`
   height: 100px;
   border: 1px solid ${(props) => props.borderColor};
   margin: 20px;
+  background-color: ${(props) => props.backgroundColor};
 `;
 
 function App() {
   return (
     <>
-      <StBox borderColor="red">빨간박스</StBox>
+      <StBox borderColor="red" backgroundColor="yellow">빨간박스</StBox>
       <StBox borderColor="blue">파란박스</StBox>
       <StBox borderColor="green">초록박스</StBox>
     </>
